e3de75ca15aa7fd768cf2a12edb5de7416ff7f74
Navneet Kumar Pathak <99376002+navneet-pathak76@users.noreply.github.com>
2026-09-13 19:33:19 +0530

    refactor(body): remove obsolete repository contract

--- apps/mobile/lib/repositories/health_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../models/body_measurement.dart';
-import '../models/health_profile.dart';
-
-abstract interface class HealthRepository {
-  Future<HealthProfile?> getProfile();
-  Future<void> saveProfile(HealthProfile profile);
-  Future<List<BodyMeasurement>> getMeasurements({DateTime? from, DateTime? to});
-  Future<void> saveMeasurement(BodyMeasurement measurement);
-}
