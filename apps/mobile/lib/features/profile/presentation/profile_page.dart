7c855f1f706359df55ca3e0b64f054cf8cefd670
Navneet Kumar Pathak <99376002+navneet-pathak76@users.noreply.github.com>
2026-09-22 13:20:02 +0530

    fix: restore Flutter build integrity

--- apps/mobile/lib/features/profile/presentation/profile_page.dart
+++ apps/mobile/lib/features/profile/presentation/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../body/presentation/body_controller.dart';
-import '../../core/widgets/sunya_glass.dart';
+import '../../body/presentation/body_controller.dart';
+import '../../../core/widgets/sunya_glass.dart';
 
 class ProfilePage extends ConsumerWidget{
  const ProfilePage({super.key});
